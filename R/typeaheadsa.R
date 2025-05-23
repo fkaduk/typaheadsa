20bd51e530b9cc4c7fc55fb646e039a2d88c5ab6
fkaduk <->
2025-05-23 19:32:08 +0200

    add placeholder function
    Style code (GHA)
    
    add function

---
+++ R/typeaheadsa.R
@@ -0,0 +1,10 @@
+#' Multiply by two
+#'
+#' This function takes a numeric input and returns double the value.
+#'
+#' @param x A numeric value.
+#' @return A numeric value, double the input.
+#' @export
+my_new_function <- function(x) {
+  x * 2
+}
